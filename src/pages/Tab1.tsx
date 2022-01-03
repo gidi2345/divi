93ace554b92876eb7c71d1f945cee4b8227e3a9d
gidi2345 <gidon1smila@gmail.com>
2022-01-03 02:35:01 +0200

    start add contacts support - first step, probaly will not work

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { Contacts } from '@capacitor-community/contacts'
 import './Tab1.css';
+
+const constants = () => {
+    let arr: any[] = [];
+     Contacts.getContacts().then(result => {
+        for (const contact of result.contacts) {
+            arr.push(<div>{contact}</div>);
+        }
+    });
+     return arr;
+}
 
 const Tab1: React.FC = () => {
   return (
@@ -11,6 +22,7 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+          {constants()}
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
